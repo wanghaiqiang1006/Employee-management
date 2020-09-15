43f1fcf53cde550b6eeea595a6334cf6b89027a8
whq <2914981334@qq.com>
2020-09-15 10:46:00 +0800

    [student information]

--- 123.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, const char *argv[])
-{
-	int a;
-	printf("%p",&a);
-	return 0;
-}
--- volite.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, const char *argv[])
-{
-	volatile unsigned int *p=(volatile unsigned int *)0xbfdfa8ac; 
-	*p=0x12345678;
-	printf("%ddddddd\n");
-	return 0;
-}
